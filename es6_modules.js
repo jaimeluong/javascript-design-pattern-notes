d86e944e59e45f5878b19623fb2146012afe48a1
Jaime Luong <jaime.luong@gmail.com>
2023-04-20 19:46:30 -0500

    Finish section

---
+++ es6_modules.js
@@ -0,0 +1,12 @@
+// A JavaScript module bundler is a tool with a build step (which has access to the file system) to create a final output that is browser-compatible
+// webpack is the bundler tool of choice for front-end developers
+
+// npm (node package manager) is a command-line tool that gives you access to plugins, libraries, and tools
+
+// src is the directory where you write all the code that webpack will bundle up
+// when webpack runs, it goes through all our files looking for any import statements then compiles all the code needed to run your site into a single file inside of the dist directory
+// the entry file is the main application file that links to all other modules in a project (/src/index.js)
+// the output file is the compiled version (dist/main.js)
+
+// There are only 2 components to the ES6 module syntax: import and export
+// By using ES6 modules, you can keep different parts of your code cleanly separated, which makes writing and maintaining your code much easier and less error-prone
